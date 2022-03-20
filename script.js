21e9f0af31bb1e89e69cccd1f53d3a335f742a50
sheiswiz <daphneruth@304gmail.com>
2022-03-20 21:49:57 +0300

    geolocation map

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 //Geolocation map
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      console.log(position);
+    });
