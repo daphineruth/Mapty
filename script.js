4383c36484e3aaf0c9e938344ca42822b2539551
sheiswiz <daphneruth@304gmail.com>
2022-03-21 10:47:25 +0300

    implementing the map

--- script.js
+++ script.js
@@ -18,8 +18,23 @@ if (navigator.geolocation)
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(latitude, longitude);
 
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-      console.log(position);
-    });
+      
+
+//implementing the map
+      var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+    },
+    function () {
+      alert('could not get your position');
+    }
+  );
